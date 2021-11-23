3c83e4040d11f553e8b8c1f4b0b235cd956581a9
I Putu Pawesi Siantika <csiantika@gmail.com>
2021-11-23 21:03:18 +0800

    menambah fungsi debouncing pada interrupt button relay dan buzzer operation.

--- lib/HEADER_FILE/HeaderFile.h
+++ lib/HEADER_FILE/HeaderFile.h
@@ -1,20 +1,25 @@
 /* this is header file.
-it includes pin definition, macros, etc.
+HeaderFile.h -> header file for initialize pin on Hadware Mini NODMCU ESP 12F
+it includes pin definition
 */
 #ifndef HEADER_FILE_H
 #define HEADER_FILE_H
 #endif
 
-/* Pzem004T pin setup */
+/* Pzem004T pins setup */
 #if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
-#define PZEM_RX_PIN D4 // it is installed to TX pin in PZEM module
-#define PZEM_TX_PIN D5 // it is installed to RX pin in Pzem module
+#define PZEM_RX_PIN D1 // it is attached to TX pin in PZEM module
+#define PZEM_TX_PIN D2 // it is attached to RX pin in Pzem module
 
-/* HwOutput class pin */
+/* HwOutput class pins */
 #define pinLEDBuzzer D8 // pin is attached to LED Buzzer status
 #define pinLEDOperation D7 // pin is attached to LED operation status 
 #define pinLEDWIFI D6 // pin is attached to LED WIFI status 
-#define pinLEDRelay D3 // pin is attached to Relay status 
+#define pinLEDRelay D5 // pin is attached to Relay status 
+
+/* InterruptHandler pins */
+#define pin_intrBuzzer D3  //pin is attached to interrupt button for Buzzer operation.
+#define pin_intrRelay D4 // pin is attached to interrupt button for Relay operation.
 
 #endif
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,24 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <PZEM004Tv30.h>
-#include <header_file.h>
+#include <HeaderFile.h>
 #include <EEPROM.h>
+#include <HwOutput.h>
 
+uint8_t GPIO_Pin = D2;
 /* Variabel initialize */
 float debit;
 float *ptr_debit = &debit;
 
-const uint8_t addr_eeprom = 0xAA; //select the address in EEPROM 
+bool stateBuzzer = 0;
+bool stateRelay = 0;
 
+unsigned long DELAY_TIME_BUTTON;
 
+const uint8_t ADDR_EEPROM = 0xAA; //select the address in EEPROM 
 
-
-
+/* Create an instance from HwOutput class*/
+HwOutput HwOut;
 
 /* Wifi setup paramater */
 const char* wifi_ssid = "Supratman2";
@@ -24,9 +29,15 @@ const char* wifi_password = "supratman231";
 /* Create an instance from LiquidCrystal_I2C.h */
  LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+
+/* Declare interrupt functions */
+void setInterruptBuzzer();
+void setInterruptRelay();
+
 /* Connect to wifi */
 void wifiConnections(); // first function declaration (for Wifi)
 /* LCD Driver and Operation*/
+
 /* Write Debit Function*/
 void writeDebit(float* debit, uint8_t  addr_eeprom_);
 float readDebit(uint8_t addr_eeprom_);
@@ -37,37 +48,35 @@ PZEM004Tv30 pzem(pzemSWSerial);
 
 void setup() {
   Serial.begin(115200); // serial communications speedbegin at 115200 
+
+  /* set up pin interrupt for buzzer and relay */
+  pinMode(pin_intrBuzzer, INPUT);
+  pinMode(pin_intrRelay, INPUT);
+
+  /* Initialize interrupt pin, function, and mode */
+  attachInterrupt(digitalPinToInterrupt(pin_intrBuzzer), setInterruptBuzzer, FALLING); // interrupt for buzzer (on/mute)
+  attachInterrupt(digitalPinToInterrupt(pin_intrRelay), setInterruptRelay, FALLING); // interrupt for relay (open / close circuit)
+
   //wifiConnections(); // function of wifi Connection
   /* LCD setup */
   //lcd.begin();
   //lcd.backlight();
   //lcd.print("* LCD SIAP *"); // print String " LCD SIAP" once (TESTING PURPOSE)
   
-  /*Turn off pin D8 (GPIO 15) */
-  pinMode(D8,OUTPUT);
-  digitalWrite(D8,LOW); // it has to do because D8 pin default settings is HIGh (save power consumption)
+  /* initialize HwOut instance(object) */
+  HwOut.initHwOutput();
+
 }
 
 void loop() {
-  *ptr_debit = 12;
-  writeDebit(ptr_debit, addr_eeprom);
-  delay(1000);
-  *ptr_debit = readDebit(addr_eeprom);
-  Serial.print("Nilai debit yang dibaca: ");
-  Serial.println(*ptr_debit);
-  delay(1000);
- 
+  Serial.println("Membaca energi dalam satuan KWH ...");
+  Serial.print("Status Buzzer: ");
+  Serial.println(stateBuzzer);
+  Serial.print("Status Relay: ");
+  Serial.println(stateRelay);
+  Serial.println("--------------------------------------------------------");
+  delay(500);
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -89,7 +98,7 @@ void wifiConnections(){
 void writeDebit(float* debit_, uint8_t addr_eeprom_){
   
   EEPROM.begin(512); // initialize addres memory used in EEPROM
-  EEPROM.write(addr_eeprom, *debit_); // write debit value in eeprom
+  EEPROM.write(ADDR_EEPROM, *debit_); // write debit value in eeprom
   EEPROM.commit(); // store data in selected memory in eeprom
   EEPROM.end(); // end the EEPROM write procces
 }
@@ -100,4 +109,26 @@ float readDebit(uint8_t addr_eeprom_){
   float debit_read = EEPROM.read(addr_eeprom_); // get value of debit in eeprom the store it to variabel 'debit_read' */
   EEPROM.end(); // end the EEPROM read procces
   return debit_read;
+}
+
+/* Interrupt function */
+// ICACHW_RAM_ATTR means the function will put in RAM not in FLASH (in esp ISR, you have to put interrupt function in RAM)
+// when buzzer interrupt button is pressed
+ICACHE_RAM_ATTR void setInterruptBuzzer(){    
+      stateBuzzer = !stateBuzzer; 
+      Serial.println("interrupt dari button buzzer"); // for testing
+      //debouncing method.
+      for (int i=0; i<2000; i++)
+      Serial.println();
+        
+  
+}
+// when relay interrupt button is pressed
+ICACHE_RAM_ATTR void setInterruptRelay(){
+    stateRelay = !stateRelay; 
+    Serial.println("interrupt dari button relay"); // for testing
+    // debouncing method
+    for (int i=0; i<2000; i++)
+      Serial.println();
+  
 }
